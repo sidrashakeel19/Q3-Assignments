89c85c5aa1afde5dcd70a90cbb5ec5d8c66bb7af
sidrashakeel19 <sidrashakeel19@hotmail.com>
2020-06-25 20:33:11 +0500

    final commit

--- Q3_Assign5/src/main.rs
+++ Q3_Assign5/src/main.rs
@@ -3,15 +3,16 @@ use actix_web::{get,post};
 use serde::Deserialize;
 
 #[get("/number/{id}")]
-async fn get_number(l: web::Path<(u32,)>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Number is {}",l.0))
+async fn get_addnumber(l: web::Path<(u32,)>) -> impl Responder {
+    let add_num : u32 = 10;
+    HttpResponse::Ok().body(format!("Number is {}",l.0 + add_num))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-        .service(get_number)
+        .service(get_addnumber)
     })
     .bind("127.0.0.1:8080")?
     .run()
